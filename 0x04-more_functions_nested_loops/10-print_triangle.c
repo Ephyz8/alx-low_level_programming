4e9e86d77661d5c6d0987e78f7f10f5ceb87b860
Ephyz8 <ziwoyaephraim@gmail.com>
2023-09-14 13:00:07 -0700

    .

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,16 +7,20 @@
 
 void print_triangle(int size)
 {
+	int i, j, k;
+
 	if (size <= 0)
 		_putchar('\n');
-
-
-	for (i = 1; i <= size; i++)
+	else
 	{
-		for (j = 1; j <= size - i; j++)
-			_putchar(' ');
+		for (i = 1; i <= size; i++)
+		{
+			for (k = size - i; k >= 1; k--)
+				_putchar(' ');
+		}
+		for (j = 1; j <= i; j++)
+			_putchar('#');
+		_putchar('\n');
 	}
-	for (j = 1; j <= i; j++)
-		_putchar('#');
-	_putchar('\n');
+
 }
