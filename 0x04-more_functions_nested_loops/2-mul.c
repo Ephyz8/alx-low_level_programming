0e6cdc8230540a7642c25faf31e667333f15a352
Ephyz8 <ziwoyaephraim@gmail.com>
2023-09-14 07:37:43 -0700

    Multiplication code

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - function to multiply two integers
+ * @a: function parameter
+ * @b: function parameter
+ * Return: 0 always
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
