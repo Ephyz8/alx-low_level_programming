ec8e999d6bd6530c1a770f147b04ed9a0373c905
Ephyz8 <ziwoyaephraim@gmail.com>
2023-09-21 08:34:02 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes string using rot13
+ * @str: string values
+ *
+ * Return: string out
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char c[] = "ABCEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char d[] = "NOPQRSTUVWXYZABCEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; c[j] != '\0'; j++)
+		{
+			if (str[i] == c[j])
+			{
+				str[i] = d[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
