0df0fe507451fcb1f6ad4793a8144eab8c4c8bfa
Ephyz8 <ziwoyaephraim@gmail.com>
2023-09-25 09:06:54 -0700

    prints the chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_chessboard - chessbboard generator
+ * @a: character
+ * Return: return 0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int row, col;
+	void _putchar(char c);
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			_putchar(a[row][col]);
+		}
+		_putchar('\n');
+	}
+}
