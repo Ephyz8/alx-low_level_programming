56390c54c91e65a8bdf2af1e23b6284b6b6829f4
Ephyz8 <ziwoyaephraim@gmail.com>
2023-09-28 13:26:19 -0700

    try

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * strlen_recur - function to check
+ * @s: parameter
+ * Return: lenght of a string
+ */
+int strlen_recur(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + strlen_recur(s + 1));
+}
+
+/**
+ * comp_string - compares string
+ * @s: parameter
+ * @t: smallest iterator
+ * @r: lagerst iterator
+ * Return: int
+ */
+int comp_string(char *s, int t, int r)
+{
+	if (*(s + t) == *(s + r))
+	{
+		if (t == r || t == r + 1)
+			return (1);
+		return (0 + comp_string(s, t + 1, r - 1));
+	}
+	return (0);
+}
+/**
+ * is_palindrome - detects palindrome
+ * @s: the string to test
+ * Return: 1 and 0
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (comp_string(s, 0, strlen_recur(s)- 1));
+}
