7f44fca6d69deafbe324c4eeb0e95e4b0c379dd5
Ephyz8 <ziwoyaephraim@gmail.com>
2023-09-28 03:30:10 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * factorial - function for factorial
+ * @n: number
+ * Return: return 1
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (n * factorial(n - 1));
+}
