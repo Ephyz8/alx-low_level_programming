209c01145bd5fcab407aeb03b5e8bfd36d7f988b
Ephyz8 <ziwoyaephraim@gmail.com>
2023-10-04 04:44:49 -0700

    trial

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,10 +19,15 @@ char *str_concat(char *s1, char *s2)
 	len1 = 0;
 	while (s1[len1] != '\0')
 		len1++;
-	for (len2 = 0; s2[len2] != '\0'; ++len2)
+	len2 = 0;
+	while (s2[len2] != '\0')
+		len2++;
 	ptr = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (ptr == NULL)
+	{
+		free(ptr);
 		return (NULL);
+	}
 	for (i = 0; i < len1; i++)
 		ptr[i] = s1[i];
 	for (j = 0; j <= len2; j++)
