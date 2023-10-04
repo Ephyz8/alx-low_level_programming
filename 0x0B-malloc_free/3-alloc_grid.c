aad57eb58a6d83ce97c9a36e58cd243932267993
Ephyz8 <ziwoyaephraim@gmail.com>
2023-10-04 05:15:19 -0700

    Matrix

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - return pointer to 2D
+ * @width: width of the array
+ * @height: height of the array
+ * Return: pointer
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j, **gridout;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	gridout = malloc(height * sizeof(int *));
+	if (gridout == NULL)
+	{
+		free(gridout);
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		gridout[i] = malloc(width * sizeof(int));
+		if (gridout[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(gridout);
+			free(gridout[i]);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			gridout[i][j] = 0;
+	return (gridout);
+}
