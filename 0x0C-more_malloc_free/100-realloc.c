ebbbe3d38b097046ca6d629927fd476383a36514
Ephyz8 <ziwoyaephraim@gmail.com>
2023-10-07 22:56:31 -0700

    New memory

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -22,8 +22,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	else if (new_size == 0)
 	{
-		return (ptr);
+		free(ptr);
+		return (NULL);
 	}
+	else if (new_size == old_size)
+		return (ptr);
+
 	p = malloc(new_size);
 	if (p == NULL)
 		return (NULL);
