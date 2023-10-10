dbb5ca72b8a09f39e917c8503042bbadc4ab1036
Ephyz8 <ziwoyaephraim@gmail.com>
2023-10-10 08:57:18 -0700

    Printing

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - print dog
+ * @d: pointer to dog
+ * Return: 0
+ */
+
+int print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name) ? d->name : "(nii)");
+		printf("Age: %f\n", (d->age) ? d->age : 0);
+		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
+	}
+	return (0);
+}
