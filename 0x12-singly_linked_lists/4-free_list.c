50e1667559984e1093f9cce2a72bba9511c0ce7e
Ephyz8 <ziwoyaephraim@gmail.com>
2023-10-19 05:00:06 -0700

    frees a list_t list.

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list.
+ * @head: pointer to the first node
+ * Return: 0
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (head->next != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+	free(head->str);
+	free(head);
+}
