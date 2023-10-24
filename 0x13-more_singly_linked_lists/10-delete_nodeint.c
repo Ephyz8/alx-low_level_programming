cae19574d8ed2ccf66f9999c553f30deb8a7caea
Ephyz8 <ziwoyaephraim@gmail.com>
2023-10-24 00:48:11 -0700

    freeing node

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes the node at index index of a list
+ * @head: pointer to the beginning of the list
+ * @index: index of node to be freed
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *ptr;
+	listint_t *node;
+
+	ptr = *head;
+	if (head == NULL || *head == NULL)
+		return (-1);
+	for (i = 0; i < index - 1 && ptr != NULL && index != 0; i++)
+		ptr = (*head)->next;
+	if (ptr == 0)
+	{
+		node = ptr->next;
+		free(ptr);
+		*head = node;
+	}
+	else
+	{
+		if (ptr->next == NULL)
+			node = ptr->next;
+		else
+			node = ptr->next->next;
+		free(ptr);
+		ptr->next = node;
+	}
+	return (1);
+}
