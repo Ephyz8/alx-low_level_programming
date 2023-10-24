eeb569f24e162ec1eb184e41843d48d8b7480b31
Ephyz8 <ziwoyaephraim@gmail.com>
2023-10-24 00:06:56 -0700

    adding a new node at any position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	ptr2->next = NULL;
 
 	idx--;
-	while (idx != 1)
+	while (idx != 0)
 	{
 		ptr = ptr->next;
 		idx--;
