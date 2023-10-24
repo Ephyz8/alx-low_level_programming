b6c32308e873d0a00509788b43cbd3df69aa907c
Ephyz8 <ziwoyaephraim@gmail.com>
2023-10-24 02:21:46 -0700

    Free node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -20,7 +20,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		ptr = ptr->next;
 	if  (ptr == NULL)
 		return (-1);
-	if (ptr == 0)
+	if (index == 0)
 	{
 		node = ptr->next;
 		free(ptr);
