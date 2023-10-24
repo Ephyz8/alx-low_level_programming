df208238749a57d8dc626d5ee085c1cae68f58f6
Ephyz8 <ziwoyaephraim@gmail.com>
2023-10-24 09:14:07 -0700

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,24 @@
 #include "lists.h"
 
 /**
+ * new_node - creates new node
+ * @n: data of the node
+ * Return: pointer to the node
+ */
+
+listint_t *new_node(int n)
+{
+	listint_t *ptr2;
+
+	ptr2 = malloc(sizeof(listint_t));
+	if (ptr2 == NULL)
+		return (NULL);
+	ptr2->n = n;
+	ptr2->next = NULL;
+	return (ptr2);
+}
+
+/**
  * insert_nodeint_at_index - inserts a new node at a given position
  * @head: first element pointer
  * @idx: index position to the new node
@@ -10,34 +28,37 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *ptr = (*head);
-	listint_t *ptr2;
+	unsigned int i;
+	listint_t *ptr;
+	listint_t *nptr;
+	listint_t *node;
 
-	ptr2 = malloc(sizeof(listint_t));
-	if (ptr2 == NULL)
-		return (NULL);
-	ptr2->n = n;
-	ptr2->next = NULL;
-
-	if (ptr == NULL)
-        {
-                ptr = ptr2;
-        }
-
-	if (idx == 0)
-		*head = ptr2;
-
-	idx--;
-	while (idx != 0)
+	ptr = *head;
+	if (head == NULL)
 	{
-		ptr = ptr->next;
-		idx--;
+		return (NULL);
 	}
-	ptr2->next = ptr->next;
-	ptr->next = ptr2;
-
-	return (ptr2);
-
-
-
+	node = new_node(n);
+	if (node == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		*head = node;
+		return (node);
+	}
+	if (idx == 0)
+		*head = node;
+	for (i = 0; i < idx - 1 && ptr != NULL && idx != 0; i++)
+		ptr = ptr->next;
+	if (ptr == NULL)
+		return (NULL);
+	if (idx == 0)
+		node->next = ptr;
+	else
+	{
+		nptr = ptr->next;
+		ptr->next = node;
+		node->next = nptr;
+	}
+	return (node);
 }
