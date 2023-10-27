2506b39f369cd891065c58d11415e24403f3592c
Ephyz8 <ziwoyaephraim@gmail.com>
2023-10-26 18:57:48 -0700

    value of a bit at a given index

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+#include <stdio.h>
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
@@ -8,5 +9,6 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
+unsigned long int _pow(unsigned int base, unsigned int power);
 
 #endif
