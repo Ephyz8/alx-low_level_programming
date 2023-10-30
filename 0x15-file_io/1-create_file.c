202a0795ea8e9cb324bc6b6f0c1ea9536cf891a3
Ephyz8 <ziwoyaephraim@gmail.com>
2023-10-30 10:57:15 -0700

    creating file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	for (letters = 0; text_content[letters]; letters++)
+	for (letters = 0; text_content[letters] != '\0'; letters++)
 		perm = write(file, text_content, letters);
 
 	if (perm == -1)
