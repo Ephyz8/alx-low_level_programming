55f2cc88b179e52ab0ca232215d00c9d602ed762
Ephyz8 <ziwoyaephraim@gmail.com>
2024-04-09 02:17:34 +0200

    Search

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,42 @@
+#include "search_algos.h"
+
+/**
+  * interpolation_search - Searches for a value in a sorted array
+  *                        of integers using interpolation search.
+  * @array: A pointer to the first element of the array to search.
+  * @size: The number of elements in the array.
+  * @value: The value to search for.
+  *
+  * Return: If the value is not present or the array is NULL, -1.
+  *         else, the first index where the value is located.
+  *
+  * Description: Prints a value every time it is compared in the array..
+  */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t j, x, y;
+
+	if (array == NULL)
+		return (-1);
+
+	for (x = 0, y = size - 1; y >= x;)
+	{
+		j = x + (((double)(y - x) / (array[y] - array[x])) * (value - array[x]));
+		if (j < size)
+			printf("Value checked array[%ld] = [%d]\n", j, array[j]);
+		else
+		{
+			printf("Value checked array[%ld] is out of range\n", j);
+			break;
+		}
+
+		if (array[j] == value)
+			return (j);
+		if (array[j] > value)
+			y = j - 1;
+		else
+			x = j + 1;
+	}
+
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,13 +1,17 @@
 #ifndef _search_algos_
 #define _search_algos_
 
-#include <stddef.h>
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t x, size_t y);
 int binary_search_index(int *array, size_t x, size_t y, int value);
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int _binary_search(int *array, size_t left, size_t right, int value);
+int exponential_search(int *array, size_t size, int value);
 
 #endif /* _search_algos_ */
